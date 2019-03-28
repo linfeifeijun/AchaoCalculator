4c8a68e29d2c4e4f79a228132a72c31e5cc16c12
linfeifeijun <2465234364@qq.com>
2019-03-29 01:05:01 +0800

    四则运算

---
+++ linfeifeijun/Calculator/Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+namespace Calculator
+{
+    public class Program
+    {
+        static public int allcount(int i, int j, int q)
+        {
+            List<char> sign = new List<char>
+            {
+                '+','-','*','/'
+            };
+            DataTable dt = new DataTable();
+            double result = Convert.ToDouble(dt.Compute((i.ToString() + sign[q] + j.ToString()), "false"));
+            if (result > 0 && Convert.ToInt32(result) == result)
+            {
+                Console.WriteLine(i.ToString() + sign[q] + j.ToString() + "=" + result.ToString() + '\r' + '\n');
+                string fname = @"E:\subject.txt";
+                if (File.Exists(fname))
+                {
+                    File.AppendAllText(fname, i.ToString() + sign[q] + j.ToString() + "=" + result.ToString());
+                }
+
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int w = 0;
+            Random a = new Random();
+            DataTable dt = new DataTable();
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (w != n)
+            {
+                int i = a.Next(0, 101);
+                int j = a.Next(0, 101);
+                int q = a.Next(0, 4);
+                allcount(i, j, q);
+                w++;
+            }
+        }
+    }
+}
---
+++ linfeifeijun/Calculator/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb3ee13f-ce4c-433e-8e12-d77b7946b75e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
